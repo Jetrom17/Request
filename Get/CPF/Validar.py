ce6e2a391e53eeded93db0d24f45bb8f85447a64
Jeiel <Jeiel17@protonmail.com>
2022-11-26 10:59:57 -0300

    Create Validar.py

---
+++ Get/CPF/Validar.py
@@ -0,0 +1,26 @@
+# Para importar o método GET que está incluído.
+
+import requests
+
+# Para importar a cor GREEN que está incluído.
+
+import sys
+
+# Armazenando dados, mais API, mais "input" e obtendo dados formato "json".
+
+req = 'https://api.invertexto.com/v1/validator?token=SEUTOKEN&value=NÚMERO'
+value = '&type=cpf'
+x = input('Ponha o CPF: ')
+dados = requests.get(req + x + value)
+print(dados.json())
+
+# Inserindo cor no texto.
+
+color = '\033[31m'
+print(green+"""
+  _____   _   _   ___    ____   __  __      _    
+ | ____| | \ | | |_ _|  / ___| |  \/  |    / \   
+ |  _|   |  \| |  | |  | |  _  | |\/| |   / _ \  
+ | |___  | |\  |  | |  | |_| | | |  | |  / ___ \ 
+ |_____| |_| \_| |___|  \____| |_|  |_| /_/   \_\
+                                                 """+green)
